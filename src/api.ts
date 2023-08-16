1b2bab1f85118c3a95668aedfe9fc3b48189ebbc
VijayManjunath <vijay.manjunath@position2.com>
2023-08-16 11:20:32 +0530

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-
+const multer = require('multer');
 export const app = express();
 
 app.use(cors({ origin: true }));
@@ -8,6 +8,19 @@ app.use(cors({ origin: true }));
 app.use(express.json());
 app.use(express.raw({ type: 'application/vnd.custom-type' }));
 app.use(express.text({ type: 'text/html' }));
+
+
+// Configure multer for file uploads
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/'); // Upload files to the 'uploads' directory
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + path.extname(file.originalname));
+    }
+});
+const upload = multer({ storage });
 
 // Healthcheck endpoint
 app.get('/', (req, res) => {
