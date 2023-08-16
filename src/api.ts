4100bb912ad8051c41b69cd14951af298d257760
VijayManjunath <vijay.manjunath@position2.com>
2023-08-16 11:21:32 +0530

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -20,7 +20,6 @@ const storage = multer.diskStorage({
         cb(null, uniqueSuffix + path.extname(file.originalname));
     }
 });
-const upload = multer({ storage });
 
 // Healthcheck endpoint
 app.get('/', (req, res) => {
